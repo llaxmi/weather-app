779cc54ecf468355ec553890e5a35dc594cc1ba6
llaxmi <ll4xmi@gmail.com>
2023-12-26 01:26:46 +0545

    add background images

---
+++ assets/bgImages/index.js
@@ -0,0 +1,9 @@
+const Morning = require("../bgImages/imgs/Morning.jpeg");
+const Rain = require("../bgImages/imgs/Rain.jpeg");
+const Night = require("../bgImages/imgs/Night.jpeg");
+const Snow = require("../bgImages/imgs/Snow.jpeg");
+const Fog = require("../bgImages/imgs/Fog.jpeg");
+const Cloud = require("../bgImages/imgs/Cloud.jpeg");
+const Normal = require("../bgImages/imgs/Normal.jpeg");
+
+export { Cloud, Fog, Morning, Night, Normal, Rain, Snow };
